b5a076183b6c6d71e8c75ed25224aed382d1661b
AddA <>
2023-03-03 19:16:28 +0800

    [init] iOS-Swift Project, setup .gitignore

---
+++ HyReadExam_Swift/HyReadExam_Swift/UserListViewController.swift
@@ -0,0 +1,21 @@
+//
+//  UserListViewController.swift
+//  HyReadExam_Swift
+//
+//  Created by AddA on 2023/3/3.
+//
+
+import UIKit
+
+class UserListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
