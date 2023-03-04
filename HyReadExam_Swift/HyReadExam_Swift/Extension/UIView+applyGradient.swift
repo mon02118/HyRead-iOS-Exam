4af1e4b4532ac5b5457d7939865b9ddc0e734609
AddA <>
2023-03-04 14:25:43 +0800

    [add] view + gradient

---
+++ HyReadExam_Swift/HyReadExam_Swift/Extension/UIView+applyGradient.swift
@@ -0,0 +1,19 @@
+//
+//  UIView+applyGradient.swift
+//  HyReadExam_Swift
+//
+//  Created by AddA on 2023/3/4.
+//
+
+import UIKit
+
+extension UIView {
+    func applyGradient(colors: [UIColor]) {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = bounds
+//        gradientLayer.startPoint = orientation.startPoint
+//        gradientLayer.endPoint = orientation.endPoint
+        gradientLayer.colors = colors.map { $0.cgColor }
+        layer.insertSublayer(gradientLayer, at: 0)
+    }
+}
